6ab769453e0ebc34d20982313beff4315c841be3
uditbh123 <uditbhattarai847@gmail.com>
2026-04-07 21:36:11 +0300

    corrected some error and added sensor data to the backend

--- readings/models.py
+++ readings/models.py
@@ -12,7 +12,7 @@ class Reading(models.Model):
     value = models.DecimalField(max_digits=6, decimal_places=2)
     unit = models.CharField(max_length=20, choices=UNIT_CHOICES)
     recorded_at = models.DateTimeField(auto_now_add=True)
-    notes = models.TextField(black=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.sensor_name} - {self.value}{self.unit}"
