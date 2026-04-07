aa6c25cbcb4cb25f9448024c83beaaf509dd6e63
uditbh123 <uditbhattarai847@gmail.com>
2026-04-07 21:23:46 +0300

    object rational mapper it maps python objects to database rows

---
+++ readings/models.py
@@ -0,0 +1,19 @@
+from django.db import models 
+
+class Reading(models.Model):
+    UNIT_CHOICES = [
+        ('pH', 'pH'),
+        ('C', 'Temperature (C)'),
+        ('mg/L', 'Concentration (mg/L)'),
+        ('%', 'Percentage (%)'),
+    ]
+
+    sensor_name = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    unit = models.CharField(max_length=20, choices=UNIT_CHOICES)
+    recorded_at = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(black=True)
+
+    def __str__(self):
+        return f"{self.sensor_name} - {self.value}{self.unit}"
+    
