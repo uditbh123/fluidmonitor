e7739a35817cc25ea6e55b2443c52476f762bf34
uditbh123 <uditbhattarai847@gmail.com>
2026-04-09 21:50:27 +0300

    updated urls.py and views.py

--- readings/urls.py
+++ readings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.add_reading, name='add_reading'),
 ]
--- readings/views.py
+++ readings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Reading 
 from django.db.models import Avg, Min, Max
+from django.shortcuts import redirect 
+from .forms import ReadingForm
 
 def home(request):
     readings = Reading.objects.all().order_by('-recorded_at')
@@ -16,3 +18,14 @@ def home(request):
         'stats': stats,
     }
     return render(request, 'readings/home.html', context)
+
+def add_reading(request):
+    if request.method == 'POST':
+        form = ReadingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = ReadingForm()
+        return render(request, 'readings/add_reading.html', {'form': form})
+    
