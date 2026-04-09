ff750f8d73e4d3beb68a1f5af062932b3be1582a
uditbh123 <uditbhattarai847@gmail.com>
2026-04-09 21:35:32 +0300

    added aggregate, context, render functions in the views.py file

--- readings/views.py
+++ readings/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Reading 
+from django.db.models import Avg, Min, Max
 
 def home(request):
-    return render(request, 'readings/home.html')
+    readings = Reading.objects.all().order_by('-recorded_at')
+
+    stats = Reading.objects.aggregate(
+        avg_value=Avg('value'),
+        min_value=Min('value'),
+        max_value=Max('value')
+    )
+
+    context = {
+        'readings': readings, 
+        'stats': stats,
+    }
+    return render(request, 'readings/home.html', context)
